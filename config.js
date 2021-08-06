5d0ffcf2113c2f33a6528267ee80876125f4f7f4
路西法 <zhaiyuxin103@hotmail.com>
2021-08-07 01:47:51 +0800

    🙈gitignore(ignore): upload config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://zhaiyuxin103:ZqDjwnjNnjGVypaT@merng.txz7z.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
